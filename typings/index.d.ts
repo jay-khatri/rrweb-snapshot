8d248dacc1dff14497454e83370a71076bc10218
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-07 19:53:05 -0400

    forgot to export thing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
 import snapshot, { recordNodes, serializeNodeWithId, transformAttribute, visitSnapshot, IGNORED_NODE } from './snapshot';
 import rebuild, { buildNodeWithSN, addHoverClass } from './rebuild';
 export * from './types';
-export { snapshot, serializeNodeWithId, rebuild, buildNodeWithSN, addHoverClass, transformAttribute, visitSnapshot, IGNORED_NODE, };
+export { recordNodes, snapshot, serializeNodeWithId, rebuild, buildNodeWithSN, addHoverClass, transformAttribute, visitSnapshot, IGNORED_NODE, };
