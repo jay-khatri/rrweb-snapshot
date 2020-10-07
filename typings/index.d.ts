5754c32990ef35471682b27dd4ffcee6579d476f
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-07 19:40:11 -0400

    more changes'

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,20 +1,4 @@
-import snapshot, {
-    recordNodes,
-    serializeNodeWithId,
-    transformAttribute,
-    visitSnapshot,
-    IGNORED_NODE,
-} from './snapshot';
+import snapshot, { recordNodes, serializeNodeWithId, transformAttribute, visitSnapshot, IGNORED_NODE } from './snapshot';
 import rebuild, { buildNodeWithSN, addHoverClass } from './rebuild';
 export * from './types';
-export {
-    recordNodes,
-    snapshot,
-    serializeNodeWithId,
-    rebuild,
-    buildNodeWithSN,
-    addHoverClass,
-    transformAttribute,
-    visitSnapshot,
-    IGNORED_NODE,
-};
+export { snapshot, serializeNodeWithId, rebuild, buildNodeWithSN, addHoverClass, transformAttribute, visitSnapshot, IGNORED_NODE, };
